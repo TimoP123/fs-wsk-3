32bab1550ec14ca360f524c8c8481c5c3a939046
Timo Pudas <timo.pudas@helsinki.fi>
2018-02-10 19:48:37 +0200

    Tehtävä 3.12

---
+++ mongo.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const url = 'mongodb://fswskdb_user:xxxxxxxx@ds229388.mlab.com:29388/fswskdb'
+
+mongoose.connect(url)
+
+const Person = mongoose.model('Person', {
+  name: String,
+  number: String
+})
+
+if (process.argv.length == 4) {
+    const person = new Person({
+    name: process.argv[2],
+    number: process.argv[3]
+    })
+
+console.log(`Lisätään henkilö ${person.name} ja numero ${person.number} luetteloon.`)
+
+person
+  .save()
+  .then(response => {
+    console.log('Henkilö tallennettiin luetteloon!')
+    mongoose.connection.close()
+  })
+} else {
+    Person.find({}).then(result => {
+        result.forEach(person => {
+            console.log(person);
+        })
+    mongoose.connection.close()
+    })
+}
